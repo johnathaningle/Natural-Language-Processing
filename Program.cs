a4910cbc3499e8f150bffa3ba16a0a6f5a585618
John Ingle <john.ingle@ihire.com>
2019-08-06 15:35:15 -0400

    Add dotnet console app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+
+namespace Natural_Language_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext();
+            char[] data = "China decided to raise".ToCharArray();
+            var dataView = mlContext.Data.LoadFromEnumerable(data)
+        }
+    }
+    public class ContentSeed
+    {
+        public string SeedText { get; set; }
+        [VectorType]
+        public int[] VariableLengthFeatures { get; set; }
+    }
+}
