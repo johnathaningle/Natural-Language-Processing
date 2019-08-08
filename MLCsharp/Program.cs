269b455c4758d95cd04556bc120e64e426bd0ee1
John Ingle <jingle1000@gmail.com>
2019-08-08 15:58:22 -0400

    Update python installation

--- MLCsharp/Program.cs
+++ MLCsharp/Program.cs
@@ -6,6 +6,7 @@ using Deedle;
 using MLCsharp.Python.Installer;
 using System.Threading.Tasks;
 using Keras.Models;
+using Python;
 
 namespace MLCsharp
 {
@@ -16,11 +17,14 @@ namespace MLCsharp
             var root = Directory.GetCurrentDirectory().Replace("\\MLCsharp", "").Replace("bin\\Debug\\netcoreapp2.2", "");
             var char2idx = getCharToIdx(root);
             var vocab = getVocab(root);
-            var installer = new Installer(root);
-            await installer.SetupPython(true);
-            var model = Sequential.ModelFromJson(File.ReadAllText(Path.Combine(root, "MLPython\\model.json")));
-            model.LoadWeight(Path.Combine(root, "MLPython\\model.h5"));
             
+        }
+        private async void runKeras(string rootDir)
+        {
+            var installer = new Installer();
+            await installer.SetupPython(true);
+            var model = Sequential.ModelFromJson(File.ReadAllText(Path.Combine(rootDir, "MLPython\\model.json")));
+            model.LoadWeight(Path.Combine(rootDir, "MLPython\\model.h5"));
         }
     }
 }
