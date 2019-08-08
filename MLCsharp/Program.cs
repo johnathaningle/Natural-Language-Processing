f946df91a873d30168464896cb4b90782f05f4a1
John Ingle <jingle1000@gmail.com>
2019-08-07 20:31:11 -0400

    Create custom installer

--- MLCsharp/Program.cs
+++ MLCsharp/Program.cs
@@ -1,39 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using static MLCsharp.Data.DataImportService;
 using Deedle;
+using MLCsharp.Python.Installer;
+using System.Threading.Tasks;
+using Keras.Models;
 
 namespace MLCsharp
 {
     class Program
     {
-        public static Dictionary<string, int> getCharToIdx(string baseDir)
-        {
-            var retval = new Dictionary<string, int>();
-            string path = Path.Combine(baseDir, "MLPython\\char2idx.csv");
-            Console.WriteLine(path);
-            var char2idx = Frame.ReadCsv(path, hasHeaders: false);
-            foreach (var val in char2idx.Rows.Values)
-            {
-                retval.Add((string)val.GetAt(0), (int)val.GetAt(1));
-            }
-            return retval;
-        }
-        public static List<string> getVocab(string baseDir)
-        {
-            var retval = new List<string>();
-            var path = Path.Combine(baseDir, "MLPython\\vocab.csv");
-            var vocab = Frame.ReadCsv(path, hasHeaders: false);
-            foreach(var val in vocab.Rows.Values) {
-                retval.Add((string)val.GetAt(1));
-            }
-            return retval;
-        }
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var root = Directory.GetCurrentDirectory().Replace("\\MLCsharp", "").Replace("bin\\Debug\\netcoreapp2.2", "");
             var char2idx = getCharToIdx(root);
             var vocab = getVocab(root);
+            var installer = new Installer(root);
+            await installer.SetupPython(true);
+            var model = Sequential.ModelFromJson(File.ReadAllText(Path.Combine(root, "MLPython\\model.json")));
+            model.LoadWeight(Path.Combine(root, "MLPython\\model.h5"));
+            
         }
     }
 }
---
+++ MLCsharp/Python.Installer/PythonEnv.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLCsharp.Python.Installer
+{
+    public static class PythonEnv
+    {
+        /// <summary>
+        /// Set PythonEnv.DeployEmbeddedPython to false to use system Python installation
+        /// </summary>
+        public static bool DeployEmbeddedPython { get; set; } = true;
+    }
+}
